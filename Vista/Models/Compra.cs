72176206cd9f85b84326586ac163de996ad47a77
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2021-01-06 14:12:51 -0600

    Update

---
+++ Vista/Models/Compra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vista.Models
+{
+    public class Compra
+    {
+        public int COM_ID { get; set; }
+        public DateTime COM_FECHA_COMPRA { get; set; }
+        public int US_ID { get; set; }
+        public string COM_LUGAR_DE_ENVIO { get; set; }
+    }
+}
